de2f4f023c129c69f5c8d4a25ad23c7c120797c2
ushioChan <1072085644@qq.com>
2020-11-22 21:58:25 +0800

    11/20

--- app/Controllers/Media.php
+++ app/Controllers/Media.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controllers;
 
 class Media extends BaseController {
--- app/Views/onehero.php
+++ app/Views/onehero.php
@@ -7,8 +7,11 @@
 		<h1>Heros Destinations</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Gender:</strong> {gender}</p>
+                <p><strong>Career:</strong> {career}</p>
+                <p><strong>Nationality:</strong> {nationality}</p>
 		<p><strong>Description:</strong> {description}</p>
-		<p><strong>Tourism site:</strong> {link}</p>
+		<p><strong>Actor:</strong> {actor}</p>
 		<p><img src="/image/{image}"/></p>
 		<p><a href="/Heros">Heros</a></p>
 	</body>
